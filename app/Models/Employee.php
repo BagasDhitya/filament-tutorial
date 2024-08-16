191e6a9088de2f8008186370f8e4b5d6170afd87
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-08-16 10:54:55 +0700

    build: added filament resources

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Employee extends Model
 {
     use HasFactory;
+    use HasApiTokens, Notifiable;
 }
