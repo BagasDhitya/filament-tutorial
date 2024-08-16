77462cd3f8a4bc625c56594a73153e18d1e563a6
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-08-16 10:12:52 +0700

    build: relational models

--- database/migrations/2024_08_16_030109_create_employees_table.php
+++ database/migrations/2024_08_16_030109_create_employees_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,17 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('country_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('state_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('department_id')->constrained()->cascadeOnDelete();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name');
+            $table->string('address');
+            $table->string('zip_code');
+            $table->string('date_of_birth');
+            $table->string('date_hired');
             $table->timestamps();
         });
     }
